f1f8399216fa6c8b3dfaa1cc99c88d8ffd1db198
Brian Dawn <brian.t.dawn@gmail.com>
2016-02-23 14:06:56 -0600

    Restructured/cleaned up code.

--- src/kademlia/core.clj
+++ src/kademlia/core.clj
@@ -1,16 +1,8 @@
 (ns kademlia.core
-  (require [aleph.udp :as udp]
-           [manifold.stream :as s]
-           [manifold.deferred :as d]
-           [taoensso.nippy :as nippy])
+  (:require [manifold.stream :as s]
+            [taoensso.nippy :as nippy]
+            [kademlia.util :as util])
   (:gen-class))
-
-(def port 1665)
-(def bind-timeout 5000) ;; milliseconds
-
-(defn timeout [] (throw (new Exception "timeout!!!")))
-(defn uuid []
-  (java.util.UUID/randomUUID))
 
 (defn send!
   "Returns a deferred that will eval to true if we successfully sent the message otherwise false."
@@ -28,61 +20,25 @@
                                          :message [the message contents]}"
   [socket msg]
   (let [{:keys [host port message]} msg
-        parsed-message (nippy/thaw message)
-        ]
+        parsed-message              (nippy/thaw message)
+        respond!                    (partial send! socket host port)]
+    
     (case (:type parsed-message)
-      :ping (send! socket host port {:type :ack})
-      )
+      :ping (respond! {:type :ack})
+      (respond! "I don't understand what you just sent.")
+      )))
 
-    )
-  )
 
-(defn bind-socket
-  "Returns a deferred socket, all messages coming into the socket will be consumed by
-   recv-handler. If port is 0 we will bind to an open port."
-  ([] (bind-socket 0))
-  ([port] (let [socket (deref (udp/socket {:port port}) bind-timeout :timeout)]
-            (when (= :timeout socket) (timeout))
-            (s/consume (partial #'recv-handler socket) socket)
-            socket)))
 
-(defonce socket (bind-socket port))
 
-(defn socket->port
-  "Get a port from a socket."
-  [socket]
-  (let [socket (:aleph/channel (meta socket))]
-    (when (nil? socket)
-      (throw (new Exception "socket is not a channel, maybe you need to defer it?")))
-    (.getPort (.localAddress socket))))
+(defn -main [& args]
+  (util/with-stream [socket (util/bind-socket #'recv-handler)]
+    (println "listening for messages on port" (util/socket->port socket))
+    
+    ;; We can do stuff here like initiate a conversation with another node!
+    ;; TODO we probably want to broadcast a hello message or something.
+    ))
 
-;; TESTS
 
-(use 'clojure.test)
 
-(defmacro with-stream
-  "Shamelessly modified with-open only it works on deferred streams instead."
-  [bindings & body]
-  (cond
-    (= (count bindings) 0) `(do ~@body)
-    (symbol? (bindings 0)) `(let ~(subvec bindings 0 2)
-                              (try
-                                (with-stream ~(subvec bindings 2) ~@body)
-                                (finally
-                                  (s/close! ~(bindings 0)))))
-    :else (throw (IllegalArgumentException.
-                   "with-stream only allows Symbols in bindings"))))
 
-(deftest ping-test
-  (testing "when we ping do we get an ack"
-    (with-stream [application-socket (bind-socket)
-                  test-socket        @(udp/socket {:port 0})
-                  
-                  ]
-      (let [application-port (socket->port application-socket)
-            application-host "localhost"
-            ]
-        (println application-host application-port (socket->port test-socket))
-        @(send! test-socket application-host application-port {:type :ping})
-        (is (= {:type :ack}
-               (nippy/thaw (:message @(s/take! test-socket)))))))))
--- test/kademlia/core_test.clj
+++ test/kademlia/core_test.clj
@@ -1,7 +1,15 @@
 (ns kademlia.core-test
   (:require [clojure.test :refer :all]
+            [aleph.udp :as udp]
+            [manifold.stream :as s]
+            [taoensso.nippy :as nippy]
+            [kademlia.util :as util]
             [kademlia.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest ping-test
+  (testing "when we ping do we get an ack"
+    (util/with-stream [application-socket (util/bind-socket #'recv-handler)
+                       test-socket        @(udp/socket {:port 0})]
+      @(send! test-socket "localhost" (util/socket->port application-socket) {:type :ping})
+      (is (= {:type :ack}
+             (nippy/thaw (:message @(s/take! test-socket))))))))
