eb4143d654a265e57ff06efe7a368a8779fc9995
Brian Dawn <brian.t.dawn@gmail.com>
2016-03-03 17:08:58 -0600

    Improved test readability and unnecessary methods.

--- src/kademlia/util.clj
+++ src/kademlia/util.clj
@@ -6,32 +6,31 @@
   "Get a port from a socket."
   [socket]
   (let [socket (:aleph/channel (meta socket))]
-    (when (nil? socket)
-      (throw (new Exception "socket is not a channel, maybe you need to defer it?")))
     (.getPort (.localAddress socket))))
 
-(defmacro with-stream
-  "Shamelessly modified with-open only it works on streams instead."
-  [bindings & body]
-  (cond
-    (= (count bindings) 0) `(do ~@body)
-    (symbol? (bindings 0)) `(let ~(subvec bindings 0 2)
-                              (try
-                                (with-stream ~(subvec bindings 2) ~@body)
-                                (finally
-                                  (s/close! ~(bindings 0)))))
-    :else (throw (IllegalArgumentException.
-                   "with-stream only allows Symbols in bindings"))))
-
-(def bind-timeout 5000) ;; milliseconds
+(def bind-timeout 1000) ;; milliseconds
 
 (defn bind-socket
   "Returns a socket, all messages coming into the socket will be consumed by
    recv-handler. If port is 0 we will bind to an open port."
-  ([recv-handler] (bind-socket recv-handler 0))
-  ([recv-handler port] (let [socket (deref (udp/socket {:port port}) bind-timeout :timeout)]
-            (when (= :timeout socket) (throw (new Exception "timed out binding socket")))
-            ;; consume will send all incoming messages to the handler. We'll pass in our socket so
-            ;; the handler can send messages back on the same socket in which the message was received.
-            (s/consume (partial recv-handler socket) socket)
-            socket)))
+
+  ;; Attempt to bind to an available port.
+  ([recv-handler]
+   (bind-socket recv-handler 0))
+
+  ;; Allow the user to specify a specific port to bind on.
+  ([recv-handler port]
+   (let [socket (deref (udp/socket {:port port})
+                       bind-timeout
+                       :timeout)]
+     
+     (when (= :timeout socket)
+       (throw (new java.util.concurrent.TimeoutException
+                   (format "timed out binding to port %s" port))))
+     
+     ;; consume will send all incoming messages to the handler. We'll pass in our socket so
+     ;; the handler can send messages back on the same socket in which the message was received.
+     (s/consume (partial recv-handler socket) socket)
+
+     ;; Return the socket.
+     socket)))
--- test/kademlia/core_test.clj
+++ test/kademlia/core_test.clj
@@ -7,9 +7,18 @@
             [kademlia.core :refer :all]))
 
 (deftest ping-test
+  (testing "do we timeout when binding to a port we shouldn't be able to bind to?"
+    (is (thrown? java.util.concurrent.TimeoutException
+                 (util/bind-socket #'recv-handler 1))))
+  
   (testing "when we ping do we get an ack"
-    (util/with-stream [application-socket (util/bind-socket #'recv-handler)
-                       test-socket        @(udp/socket {:port 0})]
-      @(send! test-socket "localhost" (util/socket->port application-socket) {:type :ping})
+    (with-open [application-socket (util/bind-socket #'recv-handler)
+                test-socket        @(udp/socket {:port 0})]
+      @(send! test-socket
+              "localhost"
+              (util/socket->port application-socket) {:type :ping})
       (is (= {:type :ack}
              (nippy/thaw (:message @(s/take! test-socket))))))))
+
+
+
