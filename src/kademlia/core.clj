c1a586e2ac0364594f43b42e4b4cd9424610114b
Brian Dawn <brian.t.dawn@gmail.com>
2016-03-03 17:55:07 -0600

    More code cleanup and tests.

--- src/kademlia/core.clj
+++ src/kademlia/core.clj
@@ -4,6 +4,8 @@
             [kademlia.util :as util])
   (:gen-class))
 
+(def do-nothing nil)
+
 (defn send!
   "Returns a deferred that will eval to true if we successfully sent the message otherwise false."
   [socket host port msg]
@@ -11,6 +13,16 @@
           {:host host
            :port port
            :message (nippy/freeze msg)}))
+
+
+(defn attempt-to-thaw-msg
+  "Returns the parsed serialized data, if we failed to parse it return nil.:
+  {:type :invalid-data}"
+  [msg]
+  (try
+    (nippy/thaw msg)
+    (catch Exception e
+      nil)))
 
 (defn recv-handler
   "Handler for all incoming messages.
@@ -20,14 +32,17 @@
                                          :message [the message contents]}"
   [socket msg]
   (let [{:keys [host port message]} msg
-        parsed-message              (try (nippy/thaw message) (catch Exception e {:type :invalid-data}))
         respond!                    (partial send! socket host port)]
-    
-    (case (:type parsed-message)
-      :ping         (respond! {:type :ack})
-      :invalid-data (respond! "hai!")
-      (respond! "Invalid message type")
-      )))
+
+    ;; If we fail parsing the message do nothing.
+    (when-let [parsed-message (attempt-to-thaw-msg (:message msg))]
+
+      (case (:type parsed-message)
+        :ping (respond! {:type :ack})
+        ;; TODO other message types here.
+        
+        ;; Do nothing if we don't understand the message.
+        nil))))
 
 (defn -main [& args]
   (let [socket (util/bind-socket #'recv-handler)]
--- test/kademlia/core_test.clj
+++ test/kademlia/core_test.clj
@@ -6,6 +6,8 @@
             [kademlia.util :as util]
             [kademlia.core :refer :all]))
 
+(def packet-response-timeout 500) ;; milliseconds
+
 (deftest ping-test
   (testing "do we timeout when binding to a port we shouldn't be able to bind to?"
     (is (thrown? java.util.concurrent.TimeoutException
@@ -18,7 +20,39 @@
               "localhost"
               (util/socket->port application-socket) {:type :ping})
       (is (= {:type :ack}
-             (nippy/thaw (:message @(s/take! test-socket))))))))
+             (nippy/thaw (:message @(s/take! test-socket)))))))
+
+  (testing "when we send an unknown message type do we get no response?"
+    (with-open [application-socket (util/bind-socket #'recv-handler)
+                test-socket        @(udp/socket {:port 0})]
+      @(send! test-socket
+              "localhost"
+              (util/socket->port application-socket) {:type :NOTAREALMESSAGETYPE})
+      (is (= :timeout
+             (deref (s/take! test-socket)
+                    packet-response-timeout
+                    :timeout)))))
+  (testing "when we send a message without a :type key do we explode?"
+    (with-open [application-socket (util/bind-socket #'recv-handler)
+                test-socket        @(udp/socket {:port 0})]
+      @(send! test-socket
+              "localhost"
+              (util/socket->port application-socket) {})
+      (is (= :timeout
+             (deref (s/take! test-socket)
+                    packet-response-timeout
+                    :timeout)))))
+  
+  (testing "when we send non valid data in the message field do we get no response?"
+    (with-open [application-socket (util/bind-socket #'recv-handler)
+                test-socket        @(udp/socket {:port 0})]
+      @(s/put! test-socket {:host "localhost"
+                           :port (util/socket->port application-socket)
+                           :message "I'm not valid serialized data! :D"})
+      (is (= :timeout
+             (deref (s/take! test-socket)
+                    packet-response-timeout
+                    :timeout))))))
 
 
 
