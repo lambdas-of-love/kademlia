df0987e8d7e73cf9eed0b84c3af07c7aae6ddfbe
Brian Dawn <brian.t.dawn@gmail.com>
2016-03-03 18:03:25 -0600

    Test readability improvements.

--- test/kademlia/core_test.clj
+++ test/kademlia/core_test.clj
@@ -8,51 +8,62 @@
 
 (def packet-response-timeout 500) ;; milliseconds
 
+(defn no-response? [stream]
+  (= :timeout
+     (deref (s/take! stream)
+            packet-response-timeout
+            :timeout)))
+
+(defn test-send!
+  "Given a test socket, and a socket that has been bound with a recv handler
+   send a message from the test socket to the socket with the handler"
+  [test-socket
+   socket-with-recv-handler
+   message]
+  @(send! test-socket
+          "localhost"
+          (util/socket->port socket-with-recv-handler)
+          message))
+
 (deftest ping-test
   (testing "do we timeout when binding to a port we shouldn't be able to bind to?"
     (is (thrown? java.util.concurrent.TimeoutException
                  (util/bind-socket #'recv-handler 1))))
   
-  (testing "when we ping do we get an ack"
+  (testing "when we ping do we get an ack?"
     (with-open [application-socket (util/bind-socket #'recv-handler)
                 test-socket        @(udp/socket {:port 0})]
-      @(send! test-socket
-              "localhost"
-              (util/socket->port application-socket) {:type :ping})
+      
+      (test-send! test-socket application-socket {:type :ping})
+      
       (is (= {:type :ack}
              (nippy/thaw (:message @(s/take! test-socket)))))))
 
   (testing "when we send an unknown message type do we get no response?"
     (with-open [application-socket (util/bind-socket #'recv-handler)
                 test-socket        @(udp/socket {:port 0})]
-      @(send! test-socket
-              "localhost"
-              (util/socket->port application-socket) {:type :NOTAREALMESSAGETYPE})
-      (is (= :timeout
-             (deref (s/take! test-socket)
-                    packet-response-timeout
-                    :timeout)))))
+
+      (test-send! test-socket application-socket {:type :NOTAREALMESSAGETYPE})
+
+      (is (no-response? test-socket))))
+  
   (testing "when we send a message without a :type key do we explode?"
     (with-open [application-socket (util/bind-socket #'recv-handler)
                 test-socket        @(udp/socket {:port 0})]
-      @(send! test-socket
-              "localhost"
-              (util/socket->port application-socket) {})
-      (is (= :timeout
-             (deref (s/take! test-socket)
-                    packet-response-timeout
-                    :timeout)))))
+
+      (test-send! test-socket application-socket {})
+
+      (is (no-response? test-socket))))
   
   (testing "when we send non valid data in the message field do we get no response?"
     (with-open [application-socket (util/bind-socket #'recv-handler)
                 test-socket        @(udp/socket {:port 0})]
+      
       @(s/put! test-socket {:host "localhost"
-                           :port (util/socket->port application-socket)
-                           :message "I'm not valid serialized data! :D"})
-      (is (= :timeout
-             (deref (s/take! test-socket)
-                    packet-response-timeout
-                    :timeout))))))
+                            :port (util/socket->port application-socket)
+                            :message "I'm not valid serialized data! :D"})
+      
+      (is (no-response? test-socket)))))
 
 
 
